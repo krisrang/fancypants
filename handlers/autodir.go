1a0e6888b11a6b42c50ea0d5f1d15773f4999845
Kristjan Rang <mail@rang.ee>
2015-10-31 18:43:10 +0200

    Sort files case insensitive. Ignore dotfiles. Switch to breadcrumbs.

--- handlers/autodir.go
+++ handlers/autodir.go
@@ -1,11 +1,11 @@
 package handlers
 
 import (
-	// "fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 
@@ -31,11 +31,15 @@ const (
 		    </div>
 		  </div>
 		</nav>
-		{{if .Up}}
-		<div class="row filerow">
-			<a href="{{.Up}}" class="col-lg-12">..</a>
-		</div>
-		{{end}}
+		<ol class="breadcrumb">
+			{{range .Breadcrumbs}}
+				{{if .Path}}
+					<li><a href="{{.Path}}">{{.Name}}</a></li>
+				{{else}}
+					<li class="active">{{.Name}}</li>
+				{{end}}
+			{{end}}
+		</ol>
 		{{range .Files}}
 			<div class="row filerow">
 				{{if .IsDir}}
@@ -69,10 +73,23 @@ func init() {
 	t = template.Must(template.New("template").Funcs(funcMap).Parse(tmpl))
 }
 
+type BreadCrumb struct {
+	Name string
+	Path string
+}
+
 type DirInfo struct {
-	Files []os.FileInfo
-	Path  string
-	Up    string
+	Files       []os.FileInfo
+	Breadcrumbs []BreadCrumb
+	Path        string
+}
+
+type ByNameCaseInsensitive []os.FileInfo
+
+func (a ByNameCaseInsensitive) Len() int      { return len(a) }
+func (a ByNameCaseInsensitive) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByNameCaseInsensitive) Less(i, j int) bool {
+	return strings.ToLower(a[i].Name()) < strings.ToLower(a[j].Name())
 }
 
 func serveIndex(w http.ResponseWriter, r *http.Request, path string) {
@@ -86,25 +103,45 @@ func serveIndex(w http.ResponseWriter, r *http.Request, path string) {
 		http.Error(w, err.Error(), 500)
 	}
 
-	up := ""
-	if urlPath != "" {
-		index := strings.LastIndex(urlPath, "/")
-		if index < 0 {
-			index = 0
+	// Sort files list by names non case sensitive
+	sort.Sort(ByNameCaseInsensitive(results))
+
+	// Build breadcrumbs
+	crumbs := make([]BreadCrumb, 0)
+	previousPath := ""
+	bits := strings.Split(r.URL.Path, "/")
+
+	for _, crumb := range bits[0 : len(bits)-1] {
+		path := previousPath + "/" + crumb
+		name := crumb
+
+		previousPath = path
+
+		if crumb == "" {
+			name = "/"
+			previousPath = ""
 		}
 
-		if index == 0 {
-			up = "/"
-		} else {
-			up = urlPath[:index]
-		}
+		crumbs = append(crumbs, BreadCrumb{
+			Name: name,
+			Path: path,
+		})
 	}
 
-	info := DirInfo{make([]os.FileInfo, 0), urlPath, up}
+	crumbs = append(crumbs, BreadCrumb{
+		Name: bits[len(bits)-1],
+	})
+
+	// List directories before files
 	files := make([]os.FileInfo, 0)
 	dirs := make([]os.FileInfo, 0)
 
 	for _, file := range results {
+		// Ignore dotfiles
+		if string(file.Name()[0]) == "." {
+			continue
+		}
+
 		if file.IsDir() {
 			dirs = append(dirs, file)
 		} else {
@@ -112,7 +149,11 @@ func serveIndex(w http.ResponseWriter, r *http.Request, path string) {
 		}
 	}
 
-	info.Files = append(dirs, files...)
+	info := DirInfo{
+		Files:       append(dirs, files...),
+		Breadcrumbs: crumbs,
+		Path:        urlPath,
+	}
 
 	w.Header().Set("Content-Type", "text/html")
 
@@ -129,6 +170,7 @@ func Autodir(path string) http.HandlerFunc {
 		if err == nil && stat.Mode().IsDir() {
 			indexPath := filepath.Join(fullPath, "index.html")
 
+			// If index.html exists, serve that
 			if _, err := os.Stat(indexPath); err == nil {
 				http.ServeFile(w, r, indexPath)
 				return
@@ -138,6 +180,7 @@ func Autodir(path string) http.HandlerFunc {
 			return
 		}
 
+		// If file is above 10MB use x-accel-redirect to have nginx serve it directly
 		if err == nil && stat.Size() > 10*1024*1024 {
 			w.Header().Set("X-Accel-Redirect", "/kris.moe"+r.URL.Path)
 		} else {
